415de3b1e95f529504e6942033735330305ebdf5
Baibhav Sureka <83425243+BaibhavSureka@users.noreply.github.com>
2024-09-11 00:27:53 +0530

    updated fonts , design

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -10,7 +10,16 @@ const Insta=()=>{
     return(
         <div className="w-screen my-5 py-5">
             
-            <span style={{fontFamily:"'Prata', serif;"}}>Follow us on Instagram</span>
+            <h4 class="text-center py-3 " style={{
+    fontFamily: "'Rockwell', 'Prata', serif",
+    textAlign: "center",
+    fontSize: "40px",
+    marginBottom: "30px",
+    fontWeight: "bold",
+    color: "#2d2d2d",
+    position: "relative"
+}}
+>Follow us on Instagram</h4>
             <div className="flex justify-center" style={{height:"40vh"}}>
                 <img src={img1} className="w-1/5 shrink" style={{width:"18%"}}></img>
                 <img src={img2} className="w-1/5 shrink" style={{width:"18%"}}></img>
